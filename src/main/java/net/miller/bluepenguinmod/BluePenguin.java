957eaa68b0d162e4093bf26d38f717320b8a057f
MCroJava <crooksiejnr@gmail.com>
2025-05-14 09:21:04 +1200

    test

---
+++ src/main/java/net/miller/bluepenguinmod/BluePenguin.java
@@ -0,0 +1,16 @@
+package net.miller.bluepenguinmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BluePenguin implements ModInitializer {
+	public static final String MOD_ID = "bluepenguinmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/miller/bluepenguinmod/BluePenguinClient.java
@@ -0,0 +1,10 @@
+package net.miller.bluepenguinmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BluePenguinClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
